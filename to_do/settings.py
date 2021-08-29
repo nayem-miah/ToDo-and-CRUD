fe8af3e74408626880c9b148788a20acbc852794
unknown <you@example.com>
2021-08-30 04:08:18 +0600

    'commited'

--- to_do/settings.py
+++ to_do/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-4#j=2r66t^)@qh7j^grn7n5o6b@ojei+!2t*ve&fi#v9nflf=i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['to-do-nayem.herokuapp.com', 'localhost', '127.0.0.1:8000/']
 
 
 # Application definition
