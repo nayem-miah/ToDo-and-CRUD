f9c69ffe9de608794afa54b066b4ae24e4f58c97
Nayem <naeemislam13790@gmail.com>
2021-12-16 16:31:19 +0600

    'commited'

--- crud/urls.py
+++ crud/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.crud, name='form'),
+    
+    ## for FBV
+    # path('', views.crud, name='form'),
+
+    #for CBV
+    path('', views.Crud_view.as_view(), name='form'),
+
     path('delete/<int:hi>/', views.delete, name='delete_data'),
     path('edit_data/<int:pk>/', views.edit_data, name='edit_data'),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,28 +1,61 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import StudentRegistation
 from .models import User
 
-def crud(request):
-    templates = 'crud.html'
-    registation = StudentRegistation()
-    if request.method == 'POST':
-        register = StudentRegistation(request.POST)
-        if register.is_valid():
-            register.save()
-            return redirect('/crud/')
-    user_data = User.objects.all()
-    context = {
-        'registation': registation,
-        'users': user_data,
-    }
 
-    return render(request, template_name=templates, context=context)
+## CRUD with FBV
+# def crud(request):
+#     templates = 'crud.html'
+#     registation = StudentRegistation()
+#     if request.method == 'POST':
+#         register = StudentRegistation(request.POST)
+#         if register.is_valid():
+#             register.save()
+#             return redirect('/crud/')
+#     user_data = User.objects.all()
+#     context = {
+#         'registation': registation,
+#         'users': user_data,
+#     }
+
+#     return render(request, template_name=templates, context=context)
+
+
+
+#CRUD with CBV
+from django.views import View
+class Crud_view(View):
+    registation = StudentRegistation
+    template_name = 'crud.html'
+    def get(self, request, *args, **kwargs):
+
+    
+        registation = self.registation()
+        user_data = User.objects.all()
+        context={
+         'registation':registation,
+         'users': user_data,
+        }
+        return render(request, self.template_name,context)
+    
+    def post(self, request, *args, **kwargs):
+        registation = self.registation(request.POST)
+        if registation.is_valid():
+           registation.save()
+           return HttpResponse("Success")
+    
+# class Delete_view(View):
+#     def get(self,request, *args, **kwargs):
+#         if request.method == "POST":
+#           pic_data = User.objects.get(id=hi)
+#           pic_data.delete()
+#           return redirect('/crud/')
 
 
 def delete(request, hi):
     if request.method == "POST":
         pic_data = User.objects.get(id=hi)
-        print(pic_data)
         pic_data.delete()
         return redirect('/crud/')
 
